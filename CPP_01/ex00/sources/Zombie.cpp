0104f90e818792a9071d88cac2d01fd9c427c2b2
Helena Utzig <helenautzig@gmail.com>
2025-03-11 15:17:29 +0200

    [BraiiiiiiinnnzzzZ]

--- CPP_01/ex00/sources/Zombie.cpp
+++ CPP_01/ex00/sources/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 10:28:07 by hutzig            #+#    #+#             */
-/*   Updated: 2025/03/11 12:02:33 by hutzig           ###   ########.fr       */
+/*   Updated: 2025/03/11 14:13:17 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,20 @@
 Zombie::Zombie(std::string name) {
 	setName(name);
 	std::cout << "Zombie " << _name << " created." << std::endl;
-	return ;
 }
 
 // Destructor
 Zombie::~Zombie() {
 	std::cout << "Zombie " << _name << " destroyed." << std::endl;
-	return ;
 }
 
 // Member function
 void		Zombie::announce() {
 	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-
 }
-
 
 void		Zombie::setName(std::string name) {
 	_name = name;
-	return ;
 }
 
 std::string	Zombie::getName() {
