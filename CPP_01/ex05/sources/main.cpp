541222ebfe90f5732ff458111df1cef8eda8f10d
Helena Utzig <helenautzig@gmail.com>
2025-03-13 19:02:47 +0200

    [Harl 2.0] swap main.cpp Harl.cpp

--- CPP_01/ex05/sources/main.cpp
+++ CPP_01/ex05/sources/main.cpp
@@ -1,49 +1,33 @@
-include "Harl.hpp"
+#include "Harl.hpp"
 
-Harl::Harl(void) {
-	_complaints[0] = &Harl::_debug;
-	_complaints[1] = &Harl::_info;
-	_complaints[2] = &Harl::_warning;
-	_complaints[3] = &Harl::_error;
-}
-
-Harl::~Harl(void) {
-	std::cout << SWAP << "[✔] Harl 2.0: Levels test completed." << RESET << std::endl << std::endl;
-}
-
-void	Harl::_debug(void) {
-	std::cout << DEBUG << std::endl;
-}
-
-void	Harl::_info(void) {
-	std::cout << INFO << std::endl;
-}
-
-void	Harl::_warning(void) {
-	std::cout << WARNING << std::endl;
-}
-
-void	Harl::_error(void) {
-	std::cout << ERROR << std::endl;
-}
-
-void	Harl::complain(std::string level) {
-	std::string levels[4] = {
-		"DEBUG",
-		"INFO",
-		"WARNING",
-		"ERROR" };
-
-	for (int i = 0; i < 4; i++)
-	{
-		if (levels[i] == level)
-		{
-			std::cout << "[HARL] ";
-			// call the corresponding member function (pointed to) using the pointer array
-			(this->*_complaints[i])();
-			return ;
-		}
-	}
-
-
+int	main(void)
+{
+	std::cout	<< std::endl
+				<< "WELCOME TO HARL 2.0 - Harl complains" << std::endl
+				<< "------------------------------------" << std::endl
+				<< std::endl;
+	
+	Harl	harl;
+	
+	std::cout	<< SWAP << "  TESTING HARL 2.0 FOR DEBUG LEVEL  " << RESET << std::endl
+				<< "------------------------------------" << std::endl;
+	harl.complain("DEBUG");
+	std::cout	<< std::endl;
+	
+	std::cout	<< SWAP << "  TESTING HARL 2.0 FOR INFO LEVEL   " << RESET << std::endl
+				<< "------------------------------------" << std::endl;
+	harl.complain("INFO");
+	std::cout	<< std::endl;
+	
+	std::cout	<< SWAP << " TESTING HARL 2.0 FOR WARNING LEVEL " << RESET << std::endl
+				<< "------------------------------------" << std::endl;
+	harl.complain("WARNING");
+	std::cout	<< std::endl;
+	
+	std::cout	<< SWAP << "  TESTING HARL 2.0 FOR ERROR LEVEL  " << RESET << std::endl
+				<< "------------------------------------" << std::endl;
+	harl.complain("ERROR");
+	std::cout	<< std::endl;
+	
+	return (0);
 }
