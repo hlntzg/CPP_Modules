55c43b29a050d3f7f7dea02456409854769efb3a
Helena Utzig <helenautzig@gmail.com>
2025-03-14 15:45:15 +0200

    [Harl filter]

--- CPP_01/ex06/includes/Harl.hpp
+++ CPP_01/ex06/includes/Harl.hpp
@@ -5,21 +5,13 @@
 #include <string>
 
 # define DEBUG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
-# define INFO "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
-# define WARNING "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
+# define INFO "I cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+# define WARNING "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month."
 # define ERROR "This is unacceptable! I want to speak to the manager now."
-
-enum	levelFilter {
-		DEBUG,
-		INFO,
-		WARNING,
-		ERROR
-};
 
 class	Harl {
 
 	private:
-		std::string	_levelFilter;
 		void		_debug(void);
 		void		_info(void);
 		void		_warning(void);
@@ -29,10 +21,7 @@ class	Harl {
 		Harl(void);
 		~Harl(void);
 		
-		void		setLevel(const std::string& input);
-		levelFilter	getLevel(void) const;
-		
-		void	complain(std::string& level);
+		void	complain(std::string level);
 };
 
 #endif
--- CPP_01/ex06/sources/Harl.cpp
+++ CPP_01/ex06/sources/Harl.cpp
@@ -1,75 +1,56 @@
 #include "Harl.hpp"
 
-Harl::Harl(void) {
-	_levelFilter = DEBUG;
-}
+Harl::Harl(void) {}
 
 Harl::~Harl(void) {
-	std::cout << "[✔] Harl Filter: Log levels completed." << std::endl;
+	std::cout << "[✔] Harl Filter: Log levels test completed." << std::endl;
 }
 
 void	Harl::_debug(void) {
-	std::cout << DEBUG << std::endl;
+	std::cout << "[ DEBUG ]\n" << DEBUG << "\n" << std::endl;
 }
 
 void	Harl::_info(void) {
-	std::cout << INFO << std::endl;
+	std::cout << "[ INFO ]\n" << INFO << "\n" << std::endl;
 }
 
 void	Harl::_warning(void) {
-	std::cout << WARNING << std::endl;
+	std::cout << "[ WARNING ]\n" << WARNING << "\n" << std::endl;
 }
 
 void	Harl::_error(void) {
-	std::cout << ERROR << std::endl;
+	std::cout << "[ ERROR ]\n" << ERROR << "\n" << std::endl;
 }
 
-void	Harl::setLevel(const std::string& input) {
-	
-	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	int			i;
+void	Harl::complain(std::string level) {
 
-	for (i = 0; i < 4; i++) {
-		if (levels[i] == input)
-			break ;
-	}
-	switch (i) {
-		case 0:
-			_levelFilter = DEBUG;
-			break ; 
-		case 1:
-			_levelFilter = INFO;
-			break ;
-		case 2:
-			_levelFilter = WARNING;
-			break ;
-		case 3:
-			_levelFilter = ERROR;
-			break ;
-	}
-}
-
-levelFilter	Harl::getLevel(void) const {
-	return (_levelFilter);
-}
-
-void		Harl::complain(std::string& level) {
-
-	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	std::string	levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	
 	void (Harl::*_complaints[4])(void) = {
-		&Harl::_debug;
-		&Harl::_info;
-		&Harl::_warning;
-		&Harl::_error;
+		&Harl::_debug,
+		&Harl::_info,
+		&Harl::_warning,
+		&Harl::_error
 	};
 
-	for (int i = _levelFilter; i < 4; i++) {
-		if (levels[i] == level)
-		{
-			std::cout << "[HARL] ";
-			(this->*_complaints[i])();
-			return ;
+	int		i;
+	for (i = 0; i < 4; i++) {
+		if (levels[i] == level) {
+			break ;
 		}
+	}
+	
+	switch (i) {
+		case 0:
+			(this->*_complaints[0])();
+		case 1:
+			(this->*_complaints[1])();
+		case 2:
+			(this->*_complaints[2])();
+		case 3:
+			(this->*_complaints[3])();
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]\n" << std::endl;
 	}
 }
