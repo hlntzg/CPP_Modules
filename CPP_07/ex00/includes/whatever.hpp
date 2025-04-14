a59e95068c304521d9476da354c8014e769bc9ee
Helena Utzig <helenautzig@gmail.com>
2025-04-14 11:07:30 +0300

    [CPP07 ex00]

--- CPP_07/ex00/includes/whatever.hpp
+++ CPP_07/ex00/includes/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 16:56:59 by hutzig            #+#    #+#             */
-/*   Updated: 2025/04/11 16:57:34 by hutzig           ###   ########.fr       */
+/*   Updated: 2025/04/14 11:04:34 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,40 @@
 # define WHATEVER_HPP
 
 
+/**
+ * swap: Swaps the values of two given parameters.
+ * Does not return anything
+ */
+template	<class T>
+void swap(T &a, T &b) {
+	T	temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+/**
+ * min:  Compares the two values passed as parameters and returns the smallest one.
+ * If they are equal, it returns the second one.
+ */
+template	<class T>
+T const &min(T &a, T &b) {
+	return (a < b ? a : b);
+}
+
+/**
+ * max: Compares the two values passed as parameters and returns the greatest one.
+ * If they are equal, it returns the second one.
+ */
+template	<class T>
+T const &max(T &a, T &b) {
+	return (a > b ? a : b);
+}
 
 #endif
+
+// The format for declaring function templates with type parameters is:
+//	- template <class identifier> function_declaration;
+//	- template <typename identifier> function_declaration;
+// Either the keyword class or the keyword typename is possible,
+// since both expressions have exactly the same meaning and behave exactly the same way.
--- CPP_07/ex00/sources/main.cpp
+++ CPP_07/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 16:57:59 by hutzig            #+#    #+#             */
-/*   Updated: 2025/04/11 16:59:49 by hutzig           ###   ########.fr       */
+/*   Updated: 2025/04/14 11:06:22 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@ int	main(void) {
 	
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
 	
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
 	
 	return (0);
 }
